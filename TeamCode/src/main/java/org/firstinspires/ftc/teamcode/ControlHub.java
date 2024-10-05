38147820f90cd93ae83eb2ea14c8d67e11ec5b77
ivy <thatjaredbrown@gmail.com>
2024-10-05 11:26:42 -0500

    TELEOP WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlHub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ControlHub.java
@@ -19,6 +19,8 @@ public class ControlHub {
         backRightMotor = map.get(DcMotor.class, "backRightMotor");
 
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        
     }
 
     public void motorAction(Gamepad gamepad) {
