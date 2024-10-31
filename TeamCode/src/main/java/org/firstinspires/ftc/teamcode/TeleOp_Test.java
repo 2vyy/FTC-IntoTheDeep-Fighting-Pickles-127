801e220b901701c7f6760a34c9a397a680ded097
ivy <thatjaredbrown@gmail.com>
2024-10-30 19:05:51 -0500

    TELEOP WORKS omg no way hes totally goated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -6,10 +6,13 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.InstantAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,18 +25,25 @@ public class TeleOp_Test extends LinearOpMode {
     public DcMotor frontRightMotor;
     public DcMotor backLeftMotor;
     public DcMotor backRightMotor;
+    public DcMotor extendArmMotor;
+    public DcMotor baseArmMotor;
+    public CRServo roller;
+    public boolean initialized = false;
+    public ElapsedTime timer = new ElapsedTime();
 
     ControlHub hub;
     private FtcDashboard dash = FtcDashboard.getInstance();
     private List<Action> runningActions = new ArrayList<>();
 
+    double powerModifier = 1;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        hub = new ControlHub();
+//        hub = new ControlHub();
         // initialPose is just here to make hub.init(HardwareMap map, Pose2d initialPose) work
         // unless you want to do roadrunner stuff in teleop :troll_face:
-        Pose2d initialPose = new Pose2d(-23, -10, Math.toRadians(0));
-        hub.init(hardwareMap, initialPose);
+//        Pose2d initialPose = new Pose2d(-23, -10, Math.toRadians(0));
+//        hub.init(hardwareMap, initialPose);
 
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
@@ -42,37 +52,80 @@ public class TeleOp_Test extends LinearOpMode {
 
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
 
+        extendArmMotor = hardwareMap.get(DcMotor.class, "extendArmMotor");
+        roller = hardwareMap.get(CRServo.class, "roller");
+
+        extendArmMotor.setTargetPosition(-130);
+        extendArmMotor.setPower(0);
+        extendArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extendArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extendArmMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
         //pauses until START is pressed
         waitForStart();
 
         while(opModeIsActive()) {
-            //rrAction();
-            //motorAction();
-            //hub.arm.armAction(gamepad1, runningActions);
+            motorAction();
+
+            telemetry.addLine("ExtendArm: "+extendArmMotor.getCurrentPosition());
+            telemetry.update();
+            armAction();
+
 
 
         }
     }
 
+    public void armAction() {
+        if(gamepad1.a) {
+            extendArmMotor.setPower(.2);
+            extendArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            extendArmMotor.setTargetPosition(-265);
+        } else if (gamepad1.b) {
+            extendArmMotor.setPower(.2);
+            extendArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            extendArmMotor.setTargetPosition(-240);
+        }
+
+        if(gamepad1.x) {
+            roller.setPower(1);
+        } else if (gamepad1.y) {
+            roller.setPower(-1);
+        } else {
+            roller.setPower(0);
+        }
+    }
+
+
+
     public void motorAction() {
         double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
         double rx = gamepad1.right_stick_x;
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), powerModifier);
         double frontLeftPower = (y + x + rx) / denominator;
         double backLeftPower = (y - x + rx) / denominator;
         double frontRightPower = (y - x - rx) / denominator;
         double backRightPower = (y + x - rx) / denominator;
+
+        if(gamepad1.back) {
+            powerModifier = 0.25;
+        } else {
+            powerModifier = 1;
+        }
 
         frontLeftMotor.setPower(frontLeftPower);
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
     }
+
+
 
     public void rrAction() {
         TelemetryPacket packet = new TelemetryPacket();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -13,38 +13,90 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 @Autonomous(name="Test Auto")
 public class TestAuto extends LinearOpMode {
+    public DcMotor frontLeftMotor;
+    public com.qualcomm.robotcore.hardware.DcMotor frontRightMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor backRightMotor;
+    public DcMotor extendArmMotor;
+    public DcMotor baseArmMotor;
+    public CRServo roller;
+    public boolean initialized = false;
+    public ElapsedTime timer = new ElapsedTime();
+
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ControlHub hub = new ControlHub();
-        Pose2d initialPose = new Pose2d(-13, -58, Math.toRadians(90)); //Position that this auto SHOULD be started at
-        hub.init(hardwareMap, initialPose); //initialPose is needed for hub.drive to be created.
 
-        // Using hub.drive.actionBuilder and the initialPose to make a roadrunner trajectory.
-        TrajectoryActionBuilder traj1 = hub.drive.actionBuilder(initialPose)
-            .splineTo(new Vector2d(-13, -33), Math.toRadians(90)); // Same code as in MeepMeep!
+//        ControlHub hub = new ControlHub();
+//        Pose2d initialPose = new Pose2d(-13, -58, Math.toRadians(90)); //Position that this auto SHOULD be started at
+//        hub.init(hardwareMap, initialPose); //initialPose is needed for hub.drive to be created.
+//
+//        // Using hub.drive.actionBuilder and the initialPose to make a roadrunner trajectory.
+//        TrajectoryActionBuilder traj1 = hub.drive.actionBuilder(initialPose)
+//            .splineTo(new Vector2d(-13, -33), Math.toRadians(90)); // Same code as in MeepMeep!
 
         //pauses until START is pressed
+
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+//        baseArmMotor = hardwareMap.get(DcMotor.class, "baseArmMotor");
+        extendArmMotor = hardwareMap.get(DcMotor.class, "extendArmMotor");
+//        roller = hardwareMap.get(CRServo.class, "roller");
+
+        extendArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        extendArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        extendArmMotor.setTargetPosition(0);
+        extendArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        extendArmMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
         waitForStart();
 
-        Actions.runBlocking(
-                //This follows traj1 and extends the armout THEN retracts the arm back
-                new SequentialAction(
-                        new ParallelAction(
-                                traj1.build(), // .build() converts the Trajectory into an Action.
-                                hub.arm.extendOut()
-                        ),
-                        hub.arm.retractBack(),
-                        new InstantAction(() -> hub.arm.stopRolling())
-                )
-        );
+        armForward();
+        while(extendArmMotor.isBusy()) {
+
+        }
+
+//        Actions.runBlocking(
+//                //This follows traj1 and extends the armout THEN retracts the arm back
+//                new SequentialAction(
+//                        new ParallelAction(
+//                                traj1.build(), // .build() converts the Trajectory into an Action.
+//                                hub.arm.extendOut()
+//                        ),
+//                        hub.arm.retractBack(),
+//                        new InstantAction(() -> hub.arm.stopRolling())
+//                )
+//        );
         
         // this might be better ??? dont look at this
         // Action trajectoryActionChosen = traj1.build();
+
+
+
+    }
+
+    public void armForward() {
+        if(!initialized) {
+            extendArmMotor.setPower(0.1);
+            extendArmMotor.setTargetPosition(-130);
+            if(extendArmMotor.getCurrentPosition()<-140) {
+                initialized = true;
+                extendArmMotor.setPower(0);
+            }
+        }
     }
 }
