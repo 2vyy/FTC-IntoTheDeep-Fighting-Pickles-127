4722b626297826172a0c3fcaf721d735dae2e753
Ogbogu Geoffrey <dreamkingmovies@gmail.com>
2025-02-04 16:31:55 -0600

    Added Auto Changes to the MeepMeep Testing including specimen

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,7 +12,7 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(600);
 
         Pose2d initialPose = new Pose2d(5, -61, Math.toRadians(90));
-        Pose2d centerBarPose = new Pose2d(5, -37, Math.toRadians(90));
+        Pose2d centerBarPose = new Pose2d(5, -33, Math.toRadians(90));
         Pose2d centerBarSecondPose = new Pose2d(30, -34, Math.toRadians(90));
         Vector2d innerSplinePose = new Vector2d(35, -20);
         Vector2d innerSplineSecondPose = new Vector2d(45.5, -15);
@@ -28,14 +28,14 @@ public class MeepMeepTesting {
         Pose2d outerSamplePushPose = new Pose2d(62, -55, Math.toRadians(90));
 
         Pose2d waitPose = new Pose2d(55, -40, Math.toRadians(90));
-        Pose2d parkPose = new Pose2d(55, -58, Math.toRadians(90));
+        Pose2d parkPose = new Pose2d(55, -61, Math.toRadians(90));
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-
+/*
         myBot.runAction(myBot.getDrive().actionBuilder(initialPose)
                 .strafeToLinearHeading(centerBarPose.position, centerBarPose.heading)
                 .strafeToLinearHeading(centerBarSecondPose.position, centerBarSecondPose.heading)
@@ -57,6 +57,45 @@ public class MeepMeepTesting {
                 .strafeToLinearHeading(parkPose.position, parkPose.heading)
 
                 .build());
+*/
+        myBot.runAction(myBot.getDrive().actionBuilder(initialPose)
+                .strafeToLinearHeading(centerBarPose.position, centerBarPose.heading)
+                .strafeTo(new Vector2d(5,-42))
+                .strafeToLinearHeading(centerBarSecondPose.position, centerBarSecondPose.heading)
+
+                .splineToConstantHeading(innerSplinePose, Math.toRadians(90))
+                .splineToConstantHeading(innerSplineSecondPose, Math.toRadians(0))
+
+                .strafeToLinearHeading(innerSamplePushPose.position, innerSamplePushPose.heading)
+                .strafeToLinearHeading(innerSamplePose.position, innerSamplePose.heading)
+                .splineToConstantHeading(centerSplinePose, Math.toRadians(270))
+
+                .strafeToLinearHeading(centerSamplePushPose.position, centerSamplePushPose.heading)
+                .strafeToLinearHeading(centerSamplePose.position, centerSamplePose.heading)
+                .splineToConstantHeading(outerSplinePose, Math.toRadians(270))
+
+                .strafeToLinearHeading(outerSamplePushPose.position, outerSamplePushPose.heading)
+
+                .strafeToLinearHeading(waitPose.position, waitPose.heading)
+                .strafeToLinearHeading(parkPose.position, parkPose.heading)
+
+                .strafeTo(new Vector2d(-4,-34))
+                //place Specimen
+                .strafeToLinearHeading(parkPose.position, parkPose.heading)
+                //pickup Specimen
+
+                .strafeTo(new Vector2d(4,-34))
+                //place Specimen
+                .strafeToLinearHeading(parkPose.position, parkPose.heading)
+                //pickup Specimen
+
+                .strafeTo(new Vector2d(8,-34))
+                //place Specimen
+                .strafeToLinearHeading(parkPose.position, parkPose.heading)
+                //pickup Specimen
+
+                .build());
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
