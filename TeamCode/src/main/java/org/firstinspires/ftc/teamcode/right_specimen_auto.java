85d110ed2da707f6b6e783a90c3b1e505cc58531
Ogbogu Geoffrey <dreamkingmovies@gmail.com>
2025-02-06 15:53:48 -0600

    Created New File Right Specimen Auto and this should only move to the car with a specimen score the specimen and back out from high bar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/right_specimen_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/right_specimen_auto.java
@@ -48,11 +48,16 @@ public class right_specimen_auto extends LinearOpMode {
         Pose2d initialPose = new Pose2d(5, -61, Math.toRadians(90));
         Pose2d centerBarPose = new Pose2d(5, -33, Math.toRadians(90));
         Pose2d centerBarSecondPose = new Pose2d(30, -34, Math.toRadians(90));
-        Vector2d innerSplinePose = new Vector2d(35, -20);
-        Vector2d innerSplineSecondPose = new Vector2d(45.5, -15);
-        Vector2d centerSplinePose = new Vector2d(56, -20);
-        Vector2d outerSplinePose = new Vector2d(62, -20);
 
+        //Vector2d innerSplinePose = new Vector2d(35, -20);
+        //Vector2d innerSplineSecondPose = new Vector2d(45.5, -15);
+        //Vector2d centerSplinePose = new Vector2d(56, -20);
+        //Vector2d outerSplinePose = new Vector2d(62, -20);
+
+        Pose2d innerSplinePose = new Pose2d(35, -20, Math.toRadians(90));
+        Pose2d innerSplineSecondPose = new Pose2d(45.5, -15, Math.toRadians(90));
+        Pose2d centerSplinePose = new Pose2d(56, -20, Math.toRadians(90));
+        Pose2d outerSplinePose = new Pose2d(62, -20, Math.toRadians(90));
 
         Pose2d innerSamplePose = new Pose2d(45.5, -12, Math.toRadians(90));
         Pose2d centerSamplePose = new Pose2d(56, -12, Math.toRadians(90));
@@ -64,6 +69,19 @@ public class right_specimen_auto extends LinearOpMode {
         Pose2d waitPose = new Pose2d(55, -40, Math.toRadians(90));
         Pose2d parkPose = new Pose2d(55, -61, Math.toRadians(90));
 
+        Pose2d backOutFromBarPose = new Pose2d(5, -42, Math.toRadians(90));
+        Pose2d towardSpecimenGroupPose = new Pose2d(35, -42,Math.toRadians(90));
+
+        //Pose2d goToFirstSpecimenPose = new Pose2d(-4, -34,Math.toRadians(90));
+        //Pose2d goToSecondSpecimenPose = new Pose2d(4, -34,Math.toRadians(90));
+        //Pose2d goToThirdSpecimenPose = new Pose2d(8, -34,Math.toRadians(90));
+
+        Pose2d placeFirstSpecimenPose = new Pose2d(-4, -34,Math.toRadians(90));
+        Pose2d placeSecondSpecimenPose = new Pose2d(4, -34,Math.toRadians(90));
+        Pose2d placeThirdSpecimenPose = new Pose2d(8, -34,Math.toRadians(90));
+
+
+
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, initialPose);
 
         TrajectoryActionBuilder goToCenter = drive.actionBuilder(initialPose)
@@ -73,6 +91,71 @@ public class right_specimen_auto extends LinearOpMode {
         TrajectoryActionBuilder outFromCenter = drive.actionBuilder(centerBarPose)
                 .strafeToConstantHeading(centerBarSecondPose.position);
         Action a_outFromCenter = outFromCenter.build();
+
+        TrajectoryActionBuilder backOutFromBar = drive.actionBuilder(centerBarSecondPose)
+                .strafeTo(backOutFromBarPose.position);
+        Action a_backOutFromBar = backOutFromBar.build();
+
+        TrajectoryActionBuilder towardSpecimenGroup = drive.actionBuilder(backOutFromBarPose)
+                .strafeToConstantHeading(new Vector2d(35,-42));
+        Action a_towardSpecimenGroup = towardSpecimenGroup.build();
+
+        TrajectoryActionBuilder goToFirstSpecimen = drive.actionBuilder(towardSpecimenGroupPose)
+                .splineToConstantHeading(innerSplinePose.position, Math.toRadians(90))
+                .splineToConstantHeading(innerSplineSecondPose.position, Math.toRadians(0));
+        Action a_goToFirstSpecimen = goToFirstSpecimen.build();
+
+        TrajectoryActionBuilder pushFirstSpecimen = drive.actionBuilder(innerSplineSecondPose)
+                .strafeToLinearHeading(innerSamplePushPose.position, innerSamplePushPose.heading)
+                .strafeToLinearHeading(innerSamplePose.position, innerSamplePose.heading);
+        Action a_pushFirstSpecimen = pushFirstSpecimen.build();
+
+        TrajectoryActionBuilder goToSecondSpecimen = drive.actionBuilder(innerSamplePose)
+                .splineToConstantHeading(centerSplinePose.position, Math.toRadians(270));
+        Action a_goToSecondSpecimen = goToSecondSpecimen.build();
+
+        TrajectoryActionBuilder pushSecondSpecimen = drive.actionBuilder(centerSplinePose)
+                .strafeToLinearHeading(centerSamplePushPose.position, centerSamplePushPose.heading)
+                .strafeToLinearHeading(centerSamplePose.position, centerSamplePose.heading);
+        Action a_pushSecondSpecimen = pushSecondSpecimen.build();
+
+        TrajectoryActionBuilder goToThirdSpecimen = drive.actionBuilder(centerSamplePose)
+                .splineToConstantHeading(outerSplinePose.position, Math.toRadians(270));
+        Action a_goToThirdSpecimen = goToThirdSpecimen.build();
+
+        TrajectoryActionBuilder pushThirdSpecimen = drive.actionBuilder(outerSplinePose)
+                .strafeToLinearHeading(outerSamplePushPose.position, outerSamplePushPose.heading);
+        Action a_pushThirdSpecimen = pushThirdSpecimen.build();
+
+        TrajectoryActionBuilder readyToPickUpSpecimen = drive.actionBuilder(outerSamplePushPose)
+                .strafeToLinearHeading(waitPose.position, waitPose.heading)
+                .strafeToLinearHeading(parkPose.position, parkPose.heading);
+        Action a_readyToPickUpSpecimen = readyToPickUpSpecimen.build();
+
+        TrajectoryActionBuilder readyToPickUpSecondSpecimen = drive.actionBuilder(placeFirstSpecimenPose)
+                .strafeToLinearHeading(parkPose.position, parkPose.heading);
+        Action a_readyToPickUpSecondSpecimen = readyToPickUpSecondSpecimen.build();
+
+        TrajectoryActionBuilder readyToPickUpThirdSpecimen = drive.actionBuilder(placeSecondSpecimenPose)
+                .strafeToLinearHeading(parkPose.position, parkPose.heading);
+        Action a_readyToPickUpThirdSpecimen = readyToPickUpThirdSpecimen.build();
+
+        TrajectoryActionBuilder finalPark = drive.actionBuilder(placeThirdSpecimenPose)
+                .strafeToLinearHeading(parkPose.position, parkPose.heading);
+        Action a_finalPark = finalPark.build();
+
+        TrajectoryActionBuilder firstSpecimen = drive.actionBuilder(centerBarPose)
+                //.strafeTo(new Vector2d(-4,-34));
+                .strafeToConstantHeading(placeFirstSpecimenPose.position);
+        Action a_firstSpecimen = firstSpecimen.build();
+
+        TrajectoryActionBuilder secondSpecimen = drive.actionBuilder(centerBarPose)
+                .strafeToConstantHeading(placeSecondSpecimenPose.position);
+        Action a_secondSpecimen = secondSpecimen.build();
+
+        TrajectoryActionBuilder thirdSpecimen = drive.actionBuilder(centerBarPose)
+                .strafeToConstantHeading(placeThirdSpecimenPose.position);
+        Action a_thirdSpecimen = thirdSpecimen.build();
 
         waitForStart();
 
@@ -92,9 +175,93 @@ public class right_specimen_auto extends LinearOpMode {
                             new SleepAction(0.35)
                         ),
                         new ParallelAction(
-                            a_outFromCenter,
+                            a_backOutFromBar,
                             slideToPosition(RobotConstants.SLIDE_REST_POS)
+                        ),
+
+                        new SequentialAction( // drop off sample
+                            a_towardSpecimenGroup,
+                                a_goToFirstSpecimen,
+                                a_pushFirstSpecimen,
+                                a_goToSecondSpecimen,
+                                a_pushSecondSpecimen,
+                                a_goToThirdSpecimen,
+                                a_pushThirdSpecimen,
+                                a_readyToPickUpSpecimen,
+                                new ServoAction(claw, RobotConstants.CLAW_CLOSE),
+                                new ServoAction(swing, RobotConstants.SWING_UP),
+                                new SleepAction(1)
+                        ),
+                        new ParallelAction(
+                                a_firstSpecimen,
+                                slideToPosition(RobotConstants.SLIDE_HIGH_BAR_POS)
+                        ),
+
+                        new SequentialAction( // drop off sample
+                                new SleepAction(0.35),
+                                new ServoAction(claw, RobotConstants.CLAW_OPEN),
+                                new SleepAction(0.35)
+                        ),
+
+                        new SequentialAction(
+                                new ServoAction(claw, RobotConstants.CLAW_CLOSE),
+                                new ServoAction(swing, RobotConstants.SWING_UP),
+                                new SleepAction(1)
+                        ),
+
+                        new ParallelAction(
+                                a_readyToPickUpSecondSpecimen,
+                                slideToPosition(RobotConstants.SLIDE_REST_POS),
+                                new SleepAction(.5)
+                        ),
+
+                        new SequentialAction(
+                                new ServoAction(claw, RobotConstants.CLAW_CLOSE),
+                                new ServoAction(swing, RobotConstants.SWING_UP),
+                                new SleepAction(1)
+                        ),
+
+                        new ParallelAction(
+                                a_secondSpecimen,
+                                slideToPosition(RobotConstants.SLIDE_HIGH_BAR_POS),
+                                new SleepAction(.5)
+                        ),
+
+                        new SequentialAction( // drop off sample
+                                new SleepAction(0.35),
+                                new ServoAction(claw, RobotConstants.CLAW_OPEN),
+                                new SleepAction(0.35)
+                        ),
+
+                        new ParallelAction(
+                                a_readyToPickUpThirdSpecimen,
+                                slideToPosition(RobotConstants.SLIDE_REST_POS),
+                                new SleepAction(.5)
+                        ),
+
+                        new SequentialAction(
+                                new ServoAction(claw, RobotConstants.CLAW_CLOSE),
+                                new ServoAction(swing, RobotConstants.SWING_UP),
+                                new SleepAction(1)
+                        ),
+
+                        new ParallelAction(
+                                a_thirdSpecimen,
+                                slideToPosition(RobotConstants.SLIDE_HIGH_BAR_POS),
+                                new SleepAction(.5)
+                        ),
+
+                        new SequentialAction( // drop off sample
+                                new SleepAction(0.35),
+                                new ServoAction(claw, RobotConstants.CLAW_OPEN),
+                                new SleepAction(0.35)
+                            ),
+
+                        new ParallelAction(
+                                a_finalPark,
+                                slideToPosition(RobotConstants.SLIDE_REST_POS)
                         )
+
                 )
         );
     }
